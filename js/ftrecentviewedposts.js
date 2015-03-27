9ca935ce36bea3f9c92a5613231f977249f8e5b8
amitaits <amitaits@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-27 07:56:07 +0000

    Removed unwanted characters from js file
    git-svn-id: https://plugins.svn.wordpress.org/posts-viewed-recently/trunk@1122013 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/ftrecentviewedposts.js
+++ js/ftrecentviewedposts.js
@@ -1,21 +1,18 @@
-nvbnjktrkrmtrmtktrjmm
-	  jQuery(document).ready(function(){
-			runcheck();
-			function runcheck(){
-				jQuery('.showthumbnail:checked').parent().next().show();
-				jQuery('.showthumbnail:not(":checked")').parent().next().hide();
-		
-		  jQuery('.showthumbnail').change(function(){
-			  
-			 	if(jQuery(this).attr('checked')) 
-					jQuery(this).parent().next().show();
-				else
-				    jQuery(this).parent().next().hide();
-			 })d ffjffb ffnhffhfhh
-			
-			}
-			 jQuery( document ).ajaxStop( function() {
-				 		runcheck();
-				} );
-			})
- nhg
+jQuery(document).ready(function() {
+    runcheck();
+	function runcheck() {
+        jQuery('.showthumbnail:checked').parent().next().show();
+        jQuery('.showthumbnail:not(":checked")').parent().next().hide();
+
+        jQuery('.showthumbnail').change(function() {
+
+            if (jQuery(this).attr('checked'))
+                jQuery(this).parent().next().show();
+            else
+                jQuery(this).parent().next().hide();
+        }) 
+	}
+    jQuery(document).ajaxStop(function() {
+        runcheck();
+    });
+})
