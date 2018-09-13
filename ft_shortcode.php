075b3a617e44c9fb693961558821086a8cede13e
Amit Sonkhiya <amit@astech.club>
2018-09-13 22:50:12 +0530

    Update ft_shortcode.php
    Code formatted in WordPress style

--- ft_shortcode.php
+++ ft_shortcode.php
@@ -1,16 +1,17 @@
 <?php
+function ft_shortcode_recentlyViewed( $atts ) {
+	// Configure defaults and extract the attributes into variables
 
-function ft_shortcode_recentlyViewed( $atts ){
-    // Configure defaults and extract the attributes into variables
+	$args = array(
+		'widget_id'    => $atts['widget_id'],
+		'by_shortcode' => 'shortcode_',
+	);
 
-    $args = array(
-        'widget_id' => $atts['widget_id'],
-        'by_shortcode' => 'shortcode_',
-    );
+	ob_start();
+	the_widget( 'ftRecentViewedPosts', '', $args );
+	$output = ob_get_clean();
 
-    ob_start();
-    the_widget( 'ftRecentViewedPosts', '', $args);
-    $output = ob_get_clean();
-    return $output;
+	return $output;
 }
+
 add_shortcode( 'ft-recentlyviewed', 'ft_shortcode_recentlyViewed' );
